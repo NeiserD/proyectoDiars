b69df78b3ade86a16826b78221204ea32d33eac3
Your Name <neydc.22@gmail.com>
2019-10-12 18:55:33 -0500

    PrimerCommit

---
+++ DIARS_1/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DIARS_1.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public int idCategoria{ get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion{ get; set; }
+        public string UnidadMedida{ get; set; }
+    }
+}
